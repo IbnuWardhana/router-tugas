241f71adf531bd816bfa14421e089143da0c21d3
IbnuWardhana <131377323+IbnuWardhana@users.noreply.github.com>
2023-06-30 02:35:21 +0700

    Add files via upload

---
+++ Home.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+      <h1>
+        {{ judul }}
+      </h1>
+    </div>
+    <div v-if="showModal">
+      <modal :header="header" :text="text" @close="openModal"/>
+    </div>
+    <button @click="openModal">open modal</button>
+  </template>
+  
+  <script>
+  
+  export default{
+    components:{
+      modal
+    },
+      data(){
+      return{
+        judul : "hello",
+        header : "Modal Content",
+        text : "ini adalah custom modal",
+        showModal : false
+      }
+    },
+    methods :{
+      handleClick(){
+        console.log(this.$refs.tes)
+        this.$refs.tes.classlist.add('ditekan')
+        this.$refs.tes.focus()
+      },
+      openModal(){
+        this.showModal = !this.showModal
+      }
+    }
+  }
+  import modal from '../components/modal.vue'
+  
+  </script>
+  <style>
+  h1{
+    border-bottom: 1px solid #ddd;
+    display : inline-block;
+    padding-bottom: 10px;
+  }
+  </style>
+  
---
+++ main.js
@@ -0,0 +1,8 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import './style/global.css'
+import router from './router'
+
+createApp(App).use(router).mount('#app')
